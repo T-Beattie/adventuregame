0ea3f4a525cae5004fb8331950d4b8480fa898b5
Tom Beattie <tombeattie12@gmail.com>
2024-04-16 15:27:07 +0100

    Add project files.

---
+++ adventuregame/adventuregame.cpp
@@ -0,0 +1,20 @@
+﻿// adventuregame.cpp : Defines the entry point for the application.
+//
+
+#include "adventuregame.h"
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+// for convenience
+using json = nlohmann::json;
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	std::ifstream f("example.json");
+	json data = json::parse(f);
+	std::cout << data.dump(4) << std::endl;
+	return 0;
+}
